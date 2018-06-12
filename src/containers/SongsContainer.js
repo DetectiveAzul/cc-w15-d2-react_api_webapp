14fbffb3a331ce0aad361a6ed3837d24aef0ab5c
Jaime Lopez <detec.azul@gmail.com>
2018-06-12 16:07:36 +0100

    Started songsContainer component

---
+++ src/containers/SongsContainer.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class SongsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blepblep = [];
+    };
+  };
+
+
+  render() {
+    <div className="songs-container">
+
+    </div>
+  }
+
+
+};
+
+
+
+export default SongsContainer;
