6665ac1e059f9d43235808275ed53f39d524972c
Jaime Lopez <detec.azul@gmail.com>
2018-06-13 09:43:52 +0100

    Final commit

--- src/components/Song.js
+++ src/components/Song.js
@@ -8,7 +8,7 @@ const Song = (props) => {
       <img alt="song cover"
         src={songObject["im:image"][2]["label"]}
       />
-      <h5>{props.order}. {props.song["im:collection"]["im:name"]["label"]}</h5>
+      <h5>{props.order + 1}. {props.song["im:collection"]["im:name"]["label"]}</h5>
       <audio controls
         style={{width: 170}}
         src={props.song["link"][1]["attributes"]["href"]}
